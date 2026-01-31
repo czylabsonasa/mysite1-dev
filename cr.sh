ac9ef1032ef4f850caa735ecbb9f5a0eee53883f
czylabsonasa <czylabson.asa@gmail.com>
2026-01-31 15:11:33 +0000

    Deploying to gh-pages from @ czylabsonasa/mysite1-dev@9a4367912bd3897860b9da3af08520039fb4cf30 🚀

--- cr.sh
+++ cr.sh
@@ -2,5 +2,5 @@ git init
 git add .
 git commit -m "first commit"
 git branch -M master
-git remote add origin git@github.com:czylabsonasa/mysite1.git
+git remote add origin git@github.com:czylabsonasa/${1}.git
 git push -u origin master
